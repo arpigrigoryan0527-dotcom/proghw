4b8b16b09cd983396b79657e6d057af1e733ea14
arpigrigoryan0527-dotcom <arpigrigoryan0527@gmail.com>
2025-09-20 13:06:17 +0400

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,12 +1,16 @@
-def bubblesort(l):
+def bubble(l):
     n=int(len(l))
     for i in range(n-1):
         for j in range(n-i-1):
             if l[j]>l[j+1]:
                 l[j], l[j+1]=l[j+1], l[j]
-
-
-def selectionsort(l):
+def insertion(l):
+    n=int(len(l))
+    for i in range(n):
+        for j in range(i,0,-1): 
+            if l[i] < l[j]:
+                l[i], l[j] = l[j], l[i]
+def selection(l):
     n=int(len(l))
     for i in range(n-1):
         k=i
@@ -14,16 +18,7 @@ def selectionsort(l):
             if l[k]>l[j]:
                 k=j
         l[k], l[i] = l[i], l[k]
-        
-def insertionsort(l):
-    n=int(len(l))
-    for i in range(n):
-        for j in range(i,0,-1): 
-            if l[i] < l[j]:
-                l[i], l[j] = l[j], l[i]
-                
-
-x = [-2, 45, 0, 11, -9]
+x = [4,7,2,4,1,0]
 bubblesort(x)
 print(x)
 selectionsort(x)
